3830bc76e707c2736fe349d7b8bae56ff977ecf5
Igor <igor@Igors-iMac.local>
2017-08-02 15:25:55 +0300

    homework

---
+++ src/main/java/prog/kiev/ua/homework/Module4/FileSystem/File.java
@@ -0,0 +1,17 @@
+package prog.kiev.ua.homework.Module4.FileSystem;
+
+/**
+ * Created by igor on 8/2/17.
+ */
+public class File {
+
+    private int size;
+
+    public File(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/main/java/prog/kiev/ua/homework/Module4/FileSystem/Runner.java
@@ -0,0 +1,26 @@
+package prog.kiev.ua.homework.Module4.FileSystem;
+
+/**
+ * Created by igor on 8/2/17.
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        Directory root = new Directory();
+
+        Directory d1 = new Directory(
+                new File(10),
+                new File(20)
+        );
+        Directory d2 = new Directory(
+                new Directory(
+                        new File(30)
+                ),
+                new File(40)
+        );
+
+        root.add(d1).add(d2).add(new File(50));
+
+        System.out.println(root.getSize());
+    }
+}
