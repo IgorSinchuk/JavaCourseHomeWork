3328ce198697362092901977fc198a2ac6ba3e95
SinchukIgor <gforce179.8@gmail.com>
2017-08-21 16:35:58 +0300

    homework

--- src/main/java/prog/kiev/ua/homework/Module7/PersonsFilter/PersonFilter.java
+++ src/main/java/prog/kiev/ua/homework/Module7/PersonsFilter/PersonFilter.java
@@ -2,6 +2,7 @@ package prog.kiev.ua.homework.Module7.PersonsFilter;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -9,35 +10,46 @@ import java.util.List;
  */
 public class PersonFilter {
 
-    private List<Person> personsList;
+    private List<Person> persons;
 
-    public PersonFilter(List<Person> personList) {
-        this.personsList = personsList;
+    public PersonFilter(List<Person> persons) {
+        this.persons = persons;
     }
 
-    public PersonFilter add(Person person) {
-        this.personsList.add(person);
+    public PersonFilter add(Person person){
+        this.persons.add(person);
         return this;
     }
 
     public PersonFilter getTeenagers() {
         List<Person> teenagers = new ArrayList<>();
-        for (Person person : this.personsList) {
-            if (person.getAge() >=12 && person.getAge() <= 17) {
+        for (Person person : this.persons) {
+            if (person.getAge() >= 12 && person.getAge() <= 17) {
                 teenagers.add(person);
             }
         }
         return new PersonFilter(teenagers);
     }
 
-    public PersonFilter sortPerson() {
-        Collections.sort(per);
+    public PersonFilter sortPersons() {
+        Collections.sort(persons, new Comparator<Person>() {
+            @Override
+            public int compare(Person person1, Person person2) {
+                if (person1.getAge() == person2.getAge()) {
+                    return person1.getName().compareTo(person2.getName());
+                }
+                return person1.getAge() - person2.getAge();
+            }
+        });
+        return this;
     }
 
     @Override
     public String toString() {
-        return "PersonFilter{" +
-                "personsList=" + personsList +
-                '}';
+        String result = "";
+        for(Person person : persons){
+            result += "\n" + person.toString();
+        }
+        return result;
     }
 }
--- src/main/java/prog/kiev/ua/homework/Module7/PersonsFilter/Runner.java
+++ src/main/java/prog/kiev/ua/homework/Module7/PersonsFilter/Runner.java
@@ -1,10 +1,19 @@
 package prog.kiev.ua.homework.Module7.PersonsFilter;
 
+import java.util.Arrays;
+
 /**
  * Created by igorsinchuk on 8/20/17.
  */
 public class Runner {
     public static void main(String[] args) {
 
+        PersonFilter personFilter = new PersonFilter(Arrays.asList(
+                new Person("Tom", 34),
+                new Person("John", 15),
+                new Person("Lisa", 16),
+                new Person("Latoya", 14)));
+        PersonFilter teenagers = personFilter.getTeenagers();
+        System.out.println(teenagers.sortPersons().toString());
     }
 }
